a0a8477e404e232fe91dfe0323bc3d65b8ea756c
ashiqur.rahman <ashiqur.rahman@spectrum-bd.com>
2025-04-30 16:44:32 +0600

    for app

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -58,7 +58,11 @@ app.use(responseTime((req, res, time) => {
 app.use(express.json({ limit: "100mb" }));
 app.use(express.urlencoded({ limit: "100mb", extended: true }));
 const corsoptions = {
-  origin: "http://localhost:4000",
+  // origin: "http://localhost:4000",
+  origin: function (origin:any, callback:any) {
+    // Allow all origins
+    callback(null, true);
+  },
   credentials: true,
   methods: ["GET", "POST", "PUT", "DELETE"],
   allowedHeaders: ["Content-Type", "Authorization"],
