e059d4277dc4cc9c4295208dee7f0035748356a0
ashiqur.rahman <ashiqur.rahman@spectrum-bd.com>
2025-05-10 20:55:48 +0600

    update secure

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -79,7 +79,7 @@ app.use(session({
     cookie: {
       maxAge: 24 * 60 * 60 * 1000, // 1 day
       httpOnly: true,
-      secure: false, // Set to true if using HTTPS
+      secure: process.env.NODE_ENV === "production", // Set to true if using HTTPS
       sameSite: "none", 
     }
 }))
